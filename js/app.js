22f93755b01dbf547e7ea6651a935e2601fa7b51
xelva <xelvacodes@gmail.com>
2022-04-07 14:21:47 -0500

    getting ready to start

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const counterObject = require('./myscript.js');
+console.log(counterObject.getCounter());
+counterObject.incrementCounter();
+console.log(counterObject.getCounter());
+
+const NewCounterObject = require('./myscript.js');
+console.log(NewCounterObject.getCounter());
+
+
---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+let counter = 0;
+module.exports = {
+    incrementCounter(){
+        counter = counter + 1;
+    },
+    getCounter() {
+        return counter;
+    }
+};
