19c2a5a794405ff629edeeb92c947a39fe90a522
xelva <xelvacodes@gmail.com>
2022-04-08 11:08:43 -0500

    done with cl tool

--- js/app.js
+++ js/app.js
@@ -1,9 +1,52 @@
-const counterObject = require('./myscript.js');
-console.log(counterObject.getCounter());
-counterObject.incrementCounter();
-console.log(counterObject.getCounter());
+#!/usr/bin/env node
+const fsPromises = require('fs/promises');
+const fs = require('fs');
+const chalk = require('chalk'); 
+const path = require('path');
 
-const NewCounterObject = require('./myscript.js');
-console.log(NewCounterObject.getCounter());
 
+
+// option 3 using fsPromises
+const { lstat } = fsPromises;
+
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
+    if (err) {
+        console.log(err);
+    } 
+    const statPromises = filenames.map(filename => {
+        return lstat(path.join(targetDir, filename));
+    });
+
+    const allStats = await Promise.all(statPromises);
+
+    for (let stats of allStats) {
+        const index = allStats.indexOf(stats);
+
+        if (stats.isFile()) {
+            console.log(chalk.green(filenames[index]))
+        } else {
+            console.log(chalk.blue(filenames[index]));
+        }
+        
+    }
+});
+
+// 1 using promise manually
+
+/* const lstat = (filename) => {
+    return new Promise((resolve, reject) => {
+        fs.lstat(filename, (err, stats) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(stats);
+        })
+    })
+} */
+
+
+// 2 using promise with util module and promisify 
+//const lstat = util.promisify(fs.lstat);
 
--- js/myscript.js
+++
@@ -1,9 +0,0 @@
-let counter = 0;
-module.exports = {
-    incrementCounter(){
-        counter = counter + 1;
-    },
-    getCounter() {
-        return counter;
-    }
-};
